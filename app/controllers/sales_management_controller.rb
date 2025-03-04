36413c610ba408cfe8026ebe1fa3e86a32b86900
s.sato <1320997git@gmail.com>
2025-03-04 19:03:50 +0900

    販売開始年月を選択できるようにする

--- app/controllers/sales_management_controller.rb
+++ app/controllers/sales_management_controller.rb
@@ -1,6 +1,6 @@
 class SalesManagementController < ApplicationController
     def index
-        # 現在の日時と比較して、販売開始されている車種を取得
-        @available_cars = Car.where('sales_start_date <= ?', Date.today)
+        selected_date = params[:selected_date] || Date.today
+        @available_cars = Car.where('sales_start_date <= ?', selected_date)
     end
 end
