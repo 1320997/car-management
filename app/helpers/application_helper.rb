eba0c867eb70f58033fcb703960e898291f48078
s.sato <1320997git@gmail.com>
2025-03-04 18:37:29 +0900

    編集画面のみで販売開始年月を登録する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    # 選択できる販売開始年月を指定する
+    def sales_date_options
+        start_date = Date.today << 3    # 3ヶ月前
+        end_date = Date.today >> 6      # 6ヶ月後
+
+        dates = (start_date..end_date).map { |d| d.beginning_of_month }.uniq
+        dates.map { |date| [date.strftime("%Y年%m月"), date] }
+    end
 end
