2ed2ce55fd0b7df359267554194c61ccc0788676
s.sato <1320997git@gmail.com>
2025-03-04 15:26:54 +0900

    車両メーカー追加画面作成

--- app/controllers/car_makers_controller.rb
+++ app/controllers/car_makers_controller.rb
@@ -1,6 +1,26 @@
 class CarMakersController < ApplicationController
-    # CarMaker一覧画面表示
+    # CarMaker 一覧画面表示
     def index
         @car_makers = CarMaker.all
+    end
+
+    # CarMaker 作成画面表示
+    def new
+        @car_maker = CarMaker.new
+    end
+
+    # CarMaker 作成処理
+    def create
+        @car_maker = CarMaker.new(car_maker_params)
+        if @car_maker.save
+            redirect_to car_makers_path
+        end
+    end
+
+    private
+    # ストロングパラメータ
+    # permitで指定した項目を許可する
+    def car_maker_params
+        params.require(:car_maker).permit(:name)
     end
 end
